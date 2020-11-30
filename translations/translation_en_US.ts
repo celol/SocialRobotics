c2528778f775dae9b80bf0506d18de17c8db9da7
Célestin HERNANDEZ <66061568+celol@users.noreply.github.com>
2020-11-30 16:39:52 +0100

    initial Commit

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello I am your friend NAO !</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello I am your friend NAO !</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello I am your friend NAO !</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello I am your friend NAO !</translation>
+        </message>
+        <message>
+            <source>Hello I am so happy to see you !</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello I am so happy to see you !</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am so happy to see you !</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am so happy to see you !</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>I am so happy to see you !</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am so happy to see you !</translation>
+        </message>
+        <message>
+            <source>I
+am at ENSTA-Paris ASR Robotics lab.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I
+am at ENSTA-Paris ASR Robotics lab.</translation>
+        </message>
+        <message>
+            <source>I
+am at ENSTA Paris ASR Robotics lab</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I
+am at ENSTA Paris ASR Robotics lab</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I'm at ENSTA Paris ASR Robotics lab</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I'm at ENSTA Paris ASR Robotics lab</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I'm at ENSTA Paris ASR Robotics lab</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I'm at ENSTA Paris ASR Robotics lab</translation>
+        </message>
+    </context>
+</TS>
